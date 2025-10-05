11b102c9fcda3e5d8d94402ef2b052bdf3a58402
EncoderIV <145885774+EncoderIV@users.noreply.github.com>
2025-10-05 12:24:26 -0400

    draft ml

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from chatterbot import ChatBot
 from openai import OpenAI 
+import pickle
+import io
 
 #init the app
 app = Flask(__name__)
@@ -13,7 +15,6 @@ app = Flask(__name__)
 
 
 # Define and init bunch of stuff
-
 class CustomChatBot(ChatBot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -45,6 +46,17 @@ movie_bot = CustomChatBot('MovieBot')
 
 
 
+#Load and init Nasa Kepler model 
+MODEL_PATH = 'models/stacking_model.pkl'
+try:
+    with open(MODEL_PATH, 'rb') as f:
+        model = pickle.load(f)
+    print(f"Model loaded from {MODEL_PATH}")
+except Exception as e:
+    print(f"Error loading model: {e}")
+
+
+EXPECTED_FEATURES = ["koi_fpflag_nt","koi_fpflag_ss","koi_fpflag_co","koi_fpflag_ec","koi_period","koi_period_err1","koi_period_err2","koi_time0bk","koi_time0bk_err1","koi_time0bk_err2","koi_impact","koi_impact_err1","koi_impact_err2","koi_duration","koi_duration_err1","koi_duration_err2","koi_depth","koi_depth_err1","koi_depth_err2","koi_prad","koi_prad_err1","koi_prad_err2","koi_teq","koi_insol","koi_insol_err1","koi_insol_err2","koi_model_snr","koi_tce_plnt_num","koi_steff","koi_steff_err1","koi_steff_err2","koi_slogg","koi_slogg_err1","koi_slogg_err2","koi_srad","koi_srad_err1","koi_srad_err2","ra","dec","koi_kepmag","koi_tce_delivnameq1_q16_tce","koi_tce_delivnameq1_q17_dr24_tce","koi_tce_delivnameq1_q17_dr25_tce","koi_disposition"]
 
 def validate_csv(file):
         
@@ -56,9 +68,8 @@ def validate_csv(file):
         return False
 
     first_row = df.columns.tolist()
-    words=["koi_fpflag_nt","koi_fpflag_ss","koi_fpflag_co","koi_fpflag_ec","koi_period","koi_period_err1","koi_period_err2","koi_time0bk","koi_time0bk_err1","koi_time0bk_err2","koi_impact","koi_impact_err1","koi_impact_err2","koi_duration","koi_duration_err1","koi_duration_err2","koi_depth","koi_depth_err1","koi_depth_err2","koi_prad","koi_prad_err1","koi_prad_err2","koi_teq","koi_insol","koi_insol_err1","koi_insol_err2","koi_model_snr","koi_tce_plnt_num","koi_steff","koi_steff_err1","koi_steff_err2","koi_slogg","koi_slogg_err1","koi_slogg_err2","koi_srad","koi_srad_err1","koi_srad_err2","ra","dec","koi_kepmag","koi_tce_delivnameq1_q16_tce","koi_tce_delivnameq1_q17_dr24_tce","koi_tce_delivnameq1_q17_dr25_tce","koi_disposition"]
     for x in first_row:
-        if(x not in words):
+        if(x not in EXPECTED_FEATURES):
             return False
         
     return True
@@ -121,6 +132,23 @@ def get_exoplanets():
     # test for plug and play once model is connected
     # will just call mdodel or read database here
     #and format here or before hand
+
+    # read csv as dataframe
+    csv_data = file.read().decode('utf-8')
+    df = pd.read_csv(io.StringIO(csv_data))
+
+    #check for NA / preprocess
+
+    X = df[EXPECTED_FEATURES] if all(col in df.columns for col in EXPECTED_FEATURES) else df
+    X = X.fillna(X.median())
+    
+    # run inference model 
+    predictions = model.predict(X)
+    probabilities = model.predict_proba(X)
+
+    #converts into JSON
+
+
     return jsonify({
         "(2023 VD3)": {
             "orbitParams": {
